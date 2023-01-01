fe769595d955056cb92ac1781e8bc1a9954f88b3
zkirby <zkirby16@gmail.com>
2022-12-31 17:07:06 -0800

    add new pages

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # personal-site
-My public personal web presence home page built with: Bun + Cloudflare Works + Notion + React + TypeScript 
+
+The home page of my public personal web presence; built with: Bun + Cloudflare Works + Notion + React + TypeScript
---
+++ next.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  reactStrictMode: true,
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "",
+        pathname: "",
+      },
+    ],
+  },
+};
