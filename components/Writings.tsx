42d70bd03cca11f1af6976da81a76edc9e9fa8f0
zkirby <zkirby16@gmail.com>
2023-01-02 20:47:41 -0800

    changes

--- components/Writings.tsx
+++ components/Writings.tsx
@@ -1,11 +1,25 @@
 import React from "react";
 
-export default function Description() {
+function WritingLink() {
+  return (
+    <div className="mt-6">
+      <h3 className="text-xl">Some text here</h3>
+      <p className="mt-1">
+        This is a brief desciption of this text as well as a reading estimate or
+        something like that
+      </p>
+    </div>
+  );
+}
+
+export default function Writings() {
   return (
     <div>
-      <div>Item 1</div>
-      <div>2022-02-03</div>
-      <div>A brief history of universal APIs</div>
+      <h1 className="text-4xl mb-6">Posts</h1>
+      <WritingLink />
+      <WritingLink />
+      <WritingLink />
+      <WritingLink />
     </div>
   );
 }
