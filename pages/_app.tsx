4c920398aa9da4c0cafd5604d856b176cad903dc
zkirby <zkirby16@gmail.com>
2023-01-07 23:56:46 -0800

    add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
+import { Analytics } from "@vercel/analytics/react";
+
 import "../dist/output.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
 
 export default MyApp;
