0b5313c8f47e044ce817107b78f7ebf791cf0756
zkirby <zkirby16@gmail.com>
2023-04-25 23:05:25 -0700

    finish touches to left and right side panels

--- components/Description.tsx
+++ components/Description.tsx
@@ -6,26 +6,25 @@ import RotatingText from "./RotatingText";
 
 export default function Description() {
   return (
-    <div>
-      <Image height="300" width="300" src={self} />
+    <div className="text-left">
+      <Image height="230" width="230" src={self} />
       <h1 className="text-4xl">Zachary Kirby</h1>
-      <p className="mt-6 text-xl">
-        <RotatingText
-          textList={["Hello", "你好", "Hola", "שלום", "Bonjour", "مرحباً"]}
-        />
-        , my name is Zach. I'm a full stack dev by trade and a hacker at heart
-        currently living in San Francisco.
-      </p>
-      <p className="mt-4 text-xl">
-        I'm currently building <a href="https://www.vessel.land/">Vessel.</a>{" "}
-        We're an engineering led company on a quest to make deep and feature
-        rich integrations with 3rd party systems a breeze.
-      </p>
-      <p className="mt-4 text-xl">
-        I love meeting new people, I think it's what makes the internet truly
-        special. Don't hesitate to reach out about anything - I almost always
-        reply!
-      </p>
+      <div className="text-left">
+        <p className="mt-6 text-xl">
+          Hello, my name is Zach. I'm a full stack dev by trade and a hacker at
+          heart currently living in San Francisco.
+        </p>
+        <p className="mt-4 text-xl">
+          I'm currently building <a href="https://www.vessel.land/">Vessel.</a>{" "}
+          We're an engineering led company on a quest to make deep and feature
+          rich integrations with 3rd party systems a breeze.
+        </p>
+        <p className="mt-4 text-xl">
+          I love meeting new people, I think it's what makes the internet truly
+          special. Don't hesitate to reach out about anything - I almost always
+          reply!
+        </p>
+      </div>
     </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,75 @@
 import React from "react";
 import Description from "../components/Description";
+import Modal from "../components/Modal";
+import { MdEmail } from "react-icons/md";
+import { BsLinkedin, BsGithub, BsTwitter } from "react-icons/bs";
 
-const PillButton = ({ text, onClick }) => {
+const PillButton = ({
+  children,
+  onClick,
+  href,
+  className,
+}: {
+  children: any;
+  onClick?: any;
+  href?: string;
+  className?: string;
+}) => {
   return (
     <button
-      className="inline-block px-6 py-2 text-lg text-white font-bol bg-pastel-green border-2 rounded-full transition duration-200 ease-out transform hover:bg-black hover:text-white hover:scale-105"
-      onClick={onClick}
+      className={`block px-6 py-2 text-xl font-bold border-2 transition duration-200 ease-out transform hover:scale-105 ${className}`}
+      onClick={href ? () => open(href, "_blank") : onClick}
     >
-      {text}
+      {children}
     </button>
   );
 };
 
 export default function Home({}) {
   return (
-    <div className="w-100 p-10">
+    <div className="p-10 overflow-hidden">
       <div className="flex m-auto w-4/5">
         <div className="basis-1/2">
           <Description />
         </div>
-        <div className="ml-40 basis-1/2">
-          <div>hello</div>
-          <iframe
-            src="https://zkirby.substack.com/embed"
-            width="480"
-            style={{ background: "black" }}
-            height="320"
-          ></iframe>
-          <div className="text-xl mt-6">
-            <a href="https://www.linkedin.com/in/zkirby/">LinkedIn</a>
-            <a href="https://github.com/zkirby">Github</a>
-            <a href="https://twitter.com/zkirby2020">Twitter</a>
-            <a href="mailto:zach@zkirby.com">Email</a>
-            <PillButton text="hello"></PillButton>
+        <div className="ml-40 basis-1/2 flex items-center justify-center h-screen">
+          <div className="flex flex-col items-center w-fit">
+            <PillButton
+              className="w-full rounded-md"
+              href="https://zkirby.substack.com/"
+            >
+              writings
+            </PillButton>
+            {/* <PillButton className="mt-6 w-full rounded-md">about</PillButton> */}
+            <div className="flex mt-6">
+              <PillButton
+                href="https://www.linkedin.com/in/zkirby/"
+                className="rounded-full px-4 py-4 mr-6 bg-pastel-lavender text-white"
+              >
+                <BsLinkedin />
+              </PillButton>
+              <PillButton
+                href="https://github.com/zkirby"
+                className="rounded-full px-4 py-4 mr-6 bg-pastel-green text-white"
+              >
+                <BsGithub />
+              </PillButton>
+              <PillButton
+                href="https://twitter.com/zkirby2020"
+                className="rounded-full px-4 py-4 mr-6 bg-pastel-blue text-white"
+              >
+                <BsTwitter />
+              </PillButton>
+              <PillButton
+                href="mailto:zach@zkirby.com"
+                className="rounded-full px-4 py-4 bg-pastel-red text-white"
+              >
+                <MdEmail />
+              </PillButton>
+            </div>
+            {/* <Modal /> */}
           </div>
+          {/* Other things to put here eventually: cool links, movies list, maybe notes. */}
         </div>
       </div>
     </div>
