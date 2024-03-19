961512750b5c72ef6e50315cd1ce9f09dbcaad99
zkirby <zkirby16@gmail.com>
2024-03-18 21:15:19 -0400

    feat: finish styling everything but the code blocks

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,4 +1,7 @@
 import type { MDXComponents } from "mdx/types";
+import cn from "classnames";
+
+const SHARED_STYLE = "font-body md:px-72 sm:px-32 leading-relaxed text-lg py-3";
 
 /**
  * We're leveraging a feature that was really designed to be used for
@@ -7,7 +10,24 @@ import type { MDXComponents } from "mdx/types";
  */
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
-    h1: ({ children }) => <h1 className="text-red-400">{children}</h1>,
+    h1: ({ children }) => (
+      <h1 className={cn(SHARED_STYLE, "font-bold text-2xl")}>{children}</h1>
+    ),
+    h3: ({ children }) => (
+      <h3 className={cn(SHARED_STYLE, "font-bold text-xl")}>{children}</h3>
+    ),
+    li: ({ children }) => (
+      <li className={cn(SHARED_STYLE, "md:px-4")}>{children}</li>
+    ),
+    ol: ({ children }) => (
+      <ol className={cn(SHARED_STYLE, "list-decimal left-5 relative")}>
+        {children}
+      </ol>
+    ),
+    p: ({ children }) => <p className={cn(SHARED_STYLE)}>{children}</p>,
+    pre: ({ children }) => (
+      <pre className={cn("bg-gray-100 md:px-72 py-4")}>{children}</pre>
+    ),
     ...components,
   };
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx}",
     "mdx-components.tsx",
   ],
   theme: {
     fontFamily: {
       sans: ["var(--font-typewriter)"],
+      body: ["Open Sans", "sans-serif"],
     },
     extend: {
       colors: {
